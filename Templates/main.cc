52a8ff5a4d03f90bcbc70ae33d7fefe3e4407444
ncl <ncl@epdt134.ph.bham.ac.uk>
2013-11-28 09:57:53 +0000

    Expanded a bit the -h help. Fixed the sighandler for proper termination

--- Templates/main.cc
+++ Templates/main.cc
@@ -9,35 +9,44 @@
 $$ANALYZERSINCLUDE$$
 using namespace std;
 
+BaseAnalysis *ban = 0;
+TApplication *theApp = 0;
+
 void usage(char* name)
 {
-	cout << "Usage: "<< name << " [-hg] < -i InputFile.root | -l InputFilesList.txt > [-B #MaxFiles] [-n #FirstEvent] [-N #Events] [-o OutputFile.root] [-v verbosity] [-p \"analyzerName:param=val;param=val&analyzerName:param=val&...\"]" << endl;
+	cout << "Usage: \t"<< name << " [-hg] < -i InputFile.root | -l InputFilesList.txt > [-B #MaxFiles] [-n #FirstEvent]" << endl;
+	cout << "\t\t[-N #Events] [-o OutputFile.root] [-v verbosity] [-p \"analyzerName:param=val;param=val&analyzerName:param=val&...\"]" << endl;
+	cout << "\t -h : Display this help" << endl;
+	cout << "\t -g : Graphical mode. Starts a ROOT application for display. Do not automatically exit at the end of the processing, Ctrl-C to exit." << endl;
+	cout << "\t -i : Path to an input ROOT file." << endl;
+	cout << "\t -l : Path to a text file containing a list of paths to input ROOT files. One file per line." << endl;
+	cout << "\t -B : Maximum number of files to process from the list." << endl;
+	cout << "\t -n : Index of the first event to process. The n-1 first events will be skipped." << endl;
+	cout << "\t -N : Maximum number of events to process." << endl;
+	cout << "\t -o : Path to output ROOT file. Will be overwritten if already exists." << endl;
+	cout << "\t -v : Verbosity level." << endl;
+	cout << "\t -p : List of parameters to pass to analyzers." << endl;
 }
 
-
-//FIXME
 void sighandler(int sig)
 {
-	if (sig == 138)
-	{
-	}
-	else
-	{
-		cerr << endl << "********************************************************************************" << endl;
-		cerr << "Killed with Signal " << sig << endl;
-		cerr << endl << "********************************************************************************" << endl;
-	}
+	cerr << endl << "********************************************************************************" << endl;
+	cerr << "Killed with Signal " << sig << endl;
+	cerr << endl << "********************************************************************************" << endl;
 	cerr << "Bye!" << endl;
+
+	delete ban;
+
+	if(theApp) theApp->Terminate();
+
 	exit(0);
 }
 
 int main(int argc, char** argv){
 	// Setting signals
 	signal(SIGXCPU, sighandler);
-	signal(SIGINT, sighandler);
 	signal(SIGTERM, sighandler);
-	signal(138, sighandler);
-	signal(127,sighandler);
+	signal(SIGINT, sighandler);
 
 	TString inFileName;
 	TString outFileName = "outFile.root";
@@ -114,11 +123,10 @@ int main(int argc, char** argv){
 		return 0;
 	}
 
-	TApplication *theApp = 0;
 	if(graphicMode) theApp = new TApplication("RecoAnalysis", &argc, argv);
 
 
-	BaseAnalysis *ban = new BaseAnalysis();
+	ban = new BaseAnalysis();
 	ban->SetVerbosity(verbosity);
 	//DEF_ANALYZER is the ClassName of the analyzer. Defined by Makefile target
 	$$ANALYZERSNEW$$
