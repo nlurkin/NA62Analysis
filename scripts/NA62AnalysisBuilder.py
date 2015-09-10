afdf8bedd07216b56558f793806264b1448629d9
nlurkin <nicolas.lurkin@gmail.com>
2015-09-10 13:20:37 +0200

    HISTORY and __rev__

--- scripts/NA62AnalysisBuilder.py
+++ scripts/NA62AnalysisBuilder.py
@@ -15,7 +15,7 @@ except ImportError:
 	from scripts.argparse import ArgumentParser, RawDescriptionHelpFormatter
 import scripts
 
-__rev__ = 569
+__rev__ = 645
 __descr__ = ("""
    Use this script when working with NA62Analysis. The script takes care of
    operations like preparing the environment, creating, renaming and cleaning 
