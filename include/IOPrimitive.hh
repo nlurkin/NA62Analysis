78a334dca720ff0c2ca93e4f25a409759c5b960d
nlurkin <nicolas.lurkin@gmail.com>
2015-11-10 18:39:12 +0100

    Add IOPrimitive class
    Not tested and not used anywhere so far

---
+++ include/IOPrimitive.hh
@@ -0,0 +1,36 @@
+/*
+ * IOPrimitive.hh
+ *
+ *  Created on: Nov 10, 2015
+ *      Author: nlurkin
+ */
+
+#ifndef INCLUDE_IOPRIMITIVE_HH_
+#define INCLUDE_IOPRIMITIVE_HH_
+
+#include <TString.h>
+
+#include "containers.hh"
+
+namespace NA62Analysis {
+namespace Core {
+
+class PrimitiveReader;
+
+class IOPrimitive {
+public:
+	IOPrimitive();
+	virtual ~IOPrimitive();
+
+	void AddReader(TString detName);
+	void AddFile(TString fileName);
+
+	PrimitiveReader* GetReader(TString detName);
+private:
+	NA62Map<TString, PrimitiveReader*>::type fReaderList;
+};
+
+} /* namespace Core */
+} /* namespace NA62Analysis */
+
+#endif /* INCLUDE_IOPRIMITIVE_HH_ */
